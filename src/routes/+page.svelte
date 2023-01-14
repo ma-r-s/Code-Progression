1b04c92886f7f96f059a090806eec5a137a5adc3
Mario Ruiz <m.ruizs@uniandes.edu.co>
2023-01-14 00:01:51 -0500

    Senior code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,25 @@
 <script>
 	import * as Tone from 'tone';
 
-	let selectedProgression = null;
 	const chords = [
 		['C4', 'G4', 'A4', 'D5'],
 		['F3', 'C4', 'G4', 'A4'],
 		['G3', 'D4', 'A4', 'E5']
 	];
+	let selectedProgression = chords[0];
 
 	function playProgression() {
 		Tone.Transport.cancel();
+		let synth = new Tone.PolySynth().toDestination();
+		new Tone.Sequence(
+			(time, note) => {
+				synth.triggerAttackRelease(note, '8n', time);
+			},
+			selectedProgression,
+			'8n'
+		).start();
 		Tone.Transport.start();
-		const synth = new Tone.Synth().toDestination();
-		const seq = new Tone.Sequence((time, note) => {
-			synth.triggerAttackRelease(note, 0.1, time);
-		}, selectedProgression).start(0);
 	}
-
 	function stopProgression() {
 		Tone.Transport.clear();
 		Tone.Transport.stop();
@@ -24,7 +27,7 @@
 </script>
 
 <div class="bg-gray-800 min-h-screen flex flex-col items-center justify-center p-4 text-center">
-	<h1 class="text-white font-bold">Chord Progression Generator</h1>
+	<h1 class="text-white font-medium">Chord Progression Generator</h1>
 	<div class="flex justify-center m-4">
 		<label for="progression" class="text-white font-medium m-2">Chord Progression:</label>
 		<select id="progression" bind:value={selectedProgression}>
@@ -33,7 +36,7 @@
 			{/each}
 		</select>
 	</div>
-	<div class="flex justify-center">
+	<div class="flex justify-center m-4">
 		<button
 			class="bg-blue-500 text-white font-bold py-2 px-4 rounded-full m-2"
 			on:click={playProgression}
